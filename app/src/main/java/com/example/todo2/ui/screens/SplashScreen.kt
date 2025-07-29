9dd0606952ec6cc41fc503c007cef4a70db05911
rtxic011 <kyukami7@gmail.com>
2025-07-29 15:23:41 +0900

    스플래쉬 화면 구현 완

--- app/src/main/java/com/example/todo2/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/todo2/ui/screens/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.todo2.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -18,7 +20,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
 import com.example.todo2.ui.theme.splashScreenBackground
@@ -41,14 +46,15 @@ fun SplashScreen(navController: NavHostController) {
             ) {
                 Column(modifier = Modifier
                     .width(198.dp)
-                    .height(154.dp)
+//                    .height(154.dp)
                     .align(Alignment.Center)
                 ) {
                     //로고
-                    Box(modifier = Modifier
-                        .size(100.dp)
-                        .align(Alignment.CenterHorizontally)
-                        .background(Color.White, shape = RoundedCornerShape(10.dp))
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .background(Color.White, shape = RoundedCornerShape(10.dp))
                     ) {
                         Icon(
                             painter = painterResource(id = R.drawable.check_icon),
@@ -68,7 +74,34 @@ fun SplashScreen(navController: NavHostController) {
 //                            .height(100.dp)
 ////                        .align(Alignment.Center)
 //                    )
+                    Spacer(modifier = Modifier.height(6.dp))
 
+                    Text(
+                        text = "To do lists",
+                        style = TextStyle(Color.White),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = "A to-do list is your compass for a more",
+                        style = TextStyle(Color.White),
+                        fontSize = 10.sp,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        text = "productive and satisfying day.",
+                        style = TextStyle(Color.White),
+                        fontSize = 10.sp,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(100.dp))
                 }
             }
         }
