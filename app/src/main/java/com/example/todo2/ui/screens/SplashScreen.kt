7ea6139af1aeb36270f51cff1f7bae35db45da72
rtxic011 <kyukami7@gmail.com>
2025-07-29 15:08:47 +0900

    스플래쉬 로고

--- app/src/main/java/com/example/todo2/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/todo2/ui/screens/SplashScreen.kt
@@ -1,9 +1,76 @@
 package com.example.todo2.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
+import com.example.todo2.ui.theme.splashScreenBackground
+import com.example.todo2.R
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+    LaunchedEffect(Unit) {
+        delay(5000)
+        navController.navigate("Theme") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+//    Scaffold(
+//        content = { innerPadding ->
+            Box(modifier = Modifier
+//                .padding(innerPadding)
+                .fillMaxSize()
+                .background(splashScreenBackground)
+            ) {
+                Column(modifier = Modifier
+                    .width(198.dp)
+                    .height(154.dp)
+                    .align(Alignment.Center)
+                ) {
+                    //로고
+                    Box(modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .background(Color.White, shape = RoundedCornerShape(10.dp))
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.check_icon),
+                            contentDescription = null,
+                            tint = splashScreenBackground,
+                            modifier = Modifier
+                                .width(77.dp)
+                                .height(70.dp)
+                                .align(Alignment.Center)
+                        )
+                    }
+//                    Icon(
+//                        painter = painterResource(id = R.drawable.splashicon),
+//                        contentDescription = null,
+//                        modifier = Modifier
+//                            .width(100.dp)
+//                            .height(100.dp)
+////                        .align(Alignment.Center)
+//                    )
 
-}
+                }
+            }
+        }
+//    )
+//}
