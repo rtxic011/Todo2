f50b48f67cfbae7c507ff10689e0a6dc1b8a53d1
rtxic011 <kyukami7@gmail.com>
2025-07-30 10:33:55 +0900

    컬러박스

--- app/src/main/java/com/example/todo2/ui/screens/ThemeScreen.kt
+++ app/src/main/java/com/example/todo2/ui/screens/ThemeScreen.kt
@@ -1,6 +1,12 @@
 package com.example.todo2.ui.screens
 
+import android.R
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import com.example.todo2.ui.theme.themered
 import com.example.todo2.ui.theme.themeyellow
@@ -8,7 +14,24 @@ import com.example.todo2.ui.theme.themegreen
 import com.example.todo2.ui.theme.themeblue
 import com.example.todo2.ui.theme.themeColor
 
+
 @Composable
 fun ThemeScreen(navController: NavHostController) {
+    var selectedColor by remember { mutableStateOf<Color?>(null) }
 
+
+    ColorBox(themeblue, false, {selectedColor = themeblue})
+    ColorBox(themegreen, false, {selectedColor = themegreen})
+    ColorBox(themered, false, {selectedColor = themered})
+    ColorBox(themeyellow, false, {selectedColor = themeyellow})
+}
+
+
+@Composable
+fun ColorBox (
+    headColor: Color,
+    isSelected : Boolean,
+    onClick: () -> Unit
+) {
+    
 }
